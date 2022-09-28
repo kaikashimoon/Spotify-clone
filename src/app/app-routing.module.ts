a611f5887c7b56262181e9ee7a3ba4a054217671
kai <leonc4667@gmail.com>
2022-09-28 15:43:40 -0400

    Angular components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '' //Todo
+  },
+  {
+    path: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ //Declarations, components, directives, pipes
     AppComponent
   ],
-  imports: [
+  imports: [ //Modules
     BrowserModule,
     AppRoutingModule
   ],
